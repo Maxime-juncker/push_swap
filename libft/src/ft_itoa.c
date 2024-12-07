35c4829c467ec8d423c68821605c3acde52c9fc3
Maxime Juncker <maxime.junck@gmail.com>
2024-12-07 11:25:26 +0100

    [fix]: remove sign check for unsigned itoa

--- libft/src/ft_itoa.c
+++ libft/src/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: mjuncker <mjuncker@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 22:04:13 by mjuncker          #+#    #+#             */
-/*   Updated: 2024/12/06 12:58:44 by mjuncker         ###   ########.fr       */
+/*   Updated: 2024/12/07 11:25:11 by mjuncker         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,28 +78,21 @@ static size_t	uget_nb_len_base(unsigned int n, int base_len)
 
 char	*u_ft_itoa(unsigned int n)
 {
-	unsigned long int	nb;
 	char				*res;
 	size_t				i;
 	size_t				len;
 
 	len = uget_nb_len_base(n, 10);
-	nb = (unsigned long)n;
 	res = malloc(len + 1);
 	i = 0;
 	if (!res)
 		return (NULL);
 	if (n == 0)
 		res[0] = '0';
-	if (nb < 0)
+	while (n > 0)
 	{
-		nb *= -1;
-		res[0] = '-';
-	}
-	while (nb > 0)
-	{
-		res[len - i++ - 1] = (nb % 10) + '0';
-		nb /= 10;
+		res[len - i++ - 1] = (n % 10) + '0';
+		n /= 10;
 	}
 	res[len] = '\0';
 	return (res);
