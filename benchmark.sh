141aed3a3cd15c709a3fb782aba4341fb868fc5d
Maxime Juncker <maxime.junck@gmail.com>
2025-01-13 12:05:40 +0100

    added average to benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -2,12 +2,14 @@
 
 AVRG=0
 MAX=0
+ITERATIONS=500
 
 make
 
-for i in $(seq 1 500);
+for i in $(seq 1 $ITERATIONS);
 do
 	COUNT=$(make rdebug | wc -l)
+	AVRG=$((AVRG + COUNT - 1))
 	if [ $[COUNT-1] -gt $MAX ]; then
 		MAX=$[COUNT-1]
 		echo -e "push_swap score: \e[31m$[COUNT-1] (new max)\e[0m"
@@ -17,5 +19,9 @@ do
 done
 
 echo -e "\e[31mmax: $MAX"
+
+# printing average
+AVRG=$(echo "scale=1; $AVRG/$ITERATIONS" | bc)
+echo -e "\e[32maverage: $AVRG"
 
 make clog
