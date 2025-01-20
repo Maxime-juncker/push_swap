dc45251cc903295e4a2899db511ccc0cc1a3d29f
Maxime Juncker <maxime.junck@gmail.com>
2025-01-20 10:53:50 +0100

    add more control to benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -2,17 +2,19 @@
 
 AVRG=0
 MAX=0
-ITERATIONS=100
+ITERATIONS=500
+N_ARGS=100
 
 make
 
-printf "\033[0m\e[?25l\n---------------------------------\n"
-printf "push_swap benchmark starting for $ITERATIONS rounds\n\n"
+printf "\033[0mstarting push_swap benchmark\n"
+printf "\033[0m\e[?25l---------------------------------\n"
+printf "\n\033[33m[ITERATION]: $ITERATIONS\n[NB_ARGS]: $N_ARGS\n\n\033[0m"
 
 
 for i in $(seq 1 $ITERATIONS);
 do
-	COUNT=$(ARG="$(python3 rand_numbers.py)"; ./bin/push_swap "$ARG" | wc -l)
+	COUNT=$(ARG="$(echo $(python3 rand_numbers.py $N_ARGS))"; ./bin/push_swap "$ARG" | wc -l)
 	AVRG=$((AVRG + COUNT))
 	if [ $[COUNT] -gt $MAX ]; then
 		MAX=$[COUNT]
--- rand_numbers.py
+++ rand_numbers.py
@@ -1,5 +1,12 @@
 import random
+import argparse
 
-random_numbers = random.sample(range(-500, 1000), 100)
-for	n in random_numbers:
-	print(n, end=" ")
+parser = argparse.ArgumentParser(description="Generate random numbers in a given range.")
+parser.add_argument("count", type=int)
+
+args = parser.parse_args()
+
+random_numbers = random.sample(range(-500, 1000), args.count)
+
+for n in random_numbers:
+    print(n, end=" ")
