4ce92179d3b7359b7cd65510ecc910ae4dbb300c
Maxime Juncker <maxime.junck@gmail.com>
2025-01-20 10:39:10 +0100

    improve formatage on benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -2,9 +2,13 @@
 
 AVRG=0
 MAX=0
-ITERATIONS=500
+ITERATIONS=100
 
 make
+
+printf "\033[0m\e[?25l\n---------------------------------\n"
+printf "push_swap benchmark starting for $ITERATIONS rounds\n\n"
+
 
 for i in $(seq 1 $ITERATIONS);
 do
@@ -12,16 +16,20 @@ do
 	AVRG=$((AVRG + COUNT))
 	if [ $[COUNT] -gt $MAX ]; then
 		MAX=$[COUNT]
-		echo -e "push_swap score: \e[31m$[COUNT] (new max)\e[0m"
+		printf "push_swap score: \e[31m$[COUNT] (new max)\e[0m\n"
 	else
-		echo -e "push_swap score: \e[32m$[COUNT]\e[0m"
+		printf "push_swap score: \e[32m$[COUNT] [i: $i]\e[0m\r"
 	fi
 done
 
-echo -e "\e[31mmax: $MAX"
+printf "\n\n\033[33m---------------------------------\n"
+printf "results:\n"
+printf "\nmax: \e[34m$MAX\n"
 
 # printing average
 AVRG=$(echo "scale=1; $AVRG/$ITERATIONS" | bc)
-echo -e "\e[32maverage: $AVRG"
+printf "\033[33maverage: \033[34m$AVRG\n"
+
+printf "\033[33m---------------------------------\n\e[?25h\n"
 
 make clog
